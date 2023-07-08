99eb2d2ab499d8bae4e087076fd7d4fce5292b95
dongcheol <ehdcjf4874@daum.net>
2023-07-09 03:37:11 +0900

    취약한 SQL문 생성

--- BBS/src/main/java/user/UserDAO.java
+++ BBS/src/main/java/user/UserDAO.java
@@ -3,6 +3,7 @@ package user;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.sql.ResultSet;
 
 public class UserDAO {
@@ -10,6 +11,7 @@ public class UserDAO {
 	private Connection conn; //자바와 데이터베이스를 연결
 	private PreparedStatement pstmt; //쿼리문 대기 및 설정
 	private ResultSet rs; //결과값 받아오기
+	private Statement stmt;
 	
 	//기본 생성자
 	//UserDAO가 실행되면 자동으로 생성되는 부분
@@ -28,22 +30,43 @@ public class UserDAO {
 	}
 //로그인 영역
 	public int login(String userID, String userPassword) {
-		String sql ="select userPassword from user where userID =?";
+//		// 안전한 쿼리문
+//		String sql ="select userPassword from user where userID = ?";
+//		try {
+//			
+//			pstmt=conn.prepareStatement(sql); //sql 쿼리문을 대기 시킨다.
+//			pstmt.setString(1, userID); // 첫번째 '?'에 매개변수로 받아온 'userId'를 대입
+//			rs=pstmt.executeQuery();
+//			if(rs.next()) {
+//				if(rs.getString(1).equals(userPassword)) 
+//					return 1; //로그인 성공
+//			else
+//					return 0; //비밀번호 틀림
+//		}
+//		return -1; //아이디 없음
+//	} catch(Exception e) {
+//		e.printStackTrace();
+//	}
+//	return -2; //오류
+//	}
+//	
+	// 취약한 쿼리문
+	// ' or '1' = '1 로그인 성공
+	// 1';# 로그인 성공
+		
+		String sql = "SELECT * FROM user WHERE userID = '" + userID + "' AND userPassword = '" + userPassword + "'";
 		try {
-			pstmt=conn.prepareStatement(sql); //sql 쿼리문을 대기 시킨다.
-			pstmt.setString(1, userID); // 첫번째 '?'에 매개변수로 받아온 'userId'를 대입
-			rs=pstmt.executeQuery();
-			if(rs.next()) {
-				if(rs.getString(1).equals(userPassword)) 
-					return 1; //로그인 성공
-			else
-					return 0; //비밀번호 틀림
+		    stmt = conn.createStatement();
+		    rs = stmt.executeQuery(sql);
+		    if (rs.next()) {
+		        return 1; // 로그인 성공
+		    } else {
+		        return 0; // 비밀번호 틀림
+		    }
+		} catch (Exception e) {
+		    e.printStackTrace();
 		}
-		return -1; //아이디 없음
-	} catch(Exception e) {
-		e.printStackTrace();
-	}
-	return -2; //오류
+		return -1; // 아이디 없음
 	}
 	
 	public int join(User user) {
